97835919fd3607da161e9db855143a0c12b45a0e
Szu <a@a.hu>
2022-12-25 13:52:59 +0000

    update

--- python/ini.py
+++ python/ini.py
@@ -8,17 +8,17 @@ client = SecretClient(
     vault_url="https://azszupee05.vault.azure.net/",
     credential=credential
 )
-secret = client.get_secret("secret2")
 
-server = client.get_secret("sqlservername") + "database.windows.net"
-database = client.get_secret("database") 
-username = client.get_secret("DBlogin")
-password = client.get_secret("dbpw")
 
-account_name = client.get_secret("storageaccount")
-account_key = client.get_secret("accountkey")
-container= client.get_secret("container")
-blobsc = client.get_secret("blobsc")
-connectionstringblob = client.get_secret("connectionstring")
+server = client.get_secret("sqlservername").value + "database.windows.net"
+database = client.get_secret("database").value
+username = client.get_secret("DBlogin").value
+password = client.get_secret("dbpw").value
+
+account_name = client.get_secret("storageaccount").value
+account_key = client.get_secret("accountkey").value
+container= client.get_secret("container").value
+blobsc = client.get_secret("blobsc").value
+connectionstringblob = client.get_secret("connectionstring").value
 
 filelocation = ''

f17a56b83d2434b3abb85d761050d2e02433bbcd
Szu <a@a.hu>
2022-12-27 18:50:50 +0000

    update

--- IaC/sql-server-instance.tf
+++ IaC/sql-server-instance.tf
@@ -14,7 +14,7 @@ resource "azurerm_sql_database" "webapponstance1" {
   resource_group_name = azurerm_resource_group.tfr-rg1.name
   location            = azurerm_resource_group.tfr-rg1.location
   server_name         = azurerm_sql_server.webappsql.name
-  edition             = "basic"
+  edition             = "Basic"
 }
 
 resource "azurerm_sql_firewall_rule" "sqlinstancerule1" {
